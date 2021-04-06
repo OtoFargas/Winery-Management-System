7970a1ae7e574c8bb810b01112c7b60e9050c78f
Lukas Fudor <fudor.lukas@gmail.com>
2021-04-06 15:32:17 +0200

    created GrapeDao interface and implementation

---
+++ src/main/java/cz/fi/muni/pa165/dao/GrapeDao.java
@@ -0,0 +1,67 @@
+package cz.fi.muni.pa165.dao;
+
+import cz.fi.muni.pa165.entity.Grape;
+import cz.fi.muni.pa165.entity.Wine;
+import cz.fi.muni.pa165.enums.GrapeColor;
+
+import java.util.List;
+
+/**
+ * Representation of DAO for Grape
+ *
+ * @author Lukáš Fudor
+ */
+
+public interface GrapeDao {
+    /**
+     * Creates persistent representation of grape entity in database
+     *
+     * @param grape to be created
+     */
+    void create(Grape grape);
+
+    /**
+     * Finds all grapes in database
+     *
+     * @return list of all grapes
+     */
+    List<Grape> findAll();
+
+    /**
+     * Finds grape by id
+     *
+     * @param id of desired grape
+     * @return found grape
+     */
+    Grape findById(Long id);
+
+    /**
+     * Finds grape by name
+     *
+     * @param name of desired grape
+     * @return found grape
+     */
+    Grape findByName(String name);
+
+    /**
+     * Finds grape by name
+     *
+     * @param color of desired grape
+     * @return list of found grapes
+     */
+    List<Grape> findByColor(GrapeColor color);
+
+    /**
+     * Removes grape from database
+     *
+     * @param grape to be deleted
+     */
+    void remove(Grape grape);
+
+    /**
+     * Updates Grape entity
+     *
+     * @param grape to be updated
+     */
+    void update(Grape grape);
+}
---
+++ src/main/java/cz/fi/muni/pa165/dao/GrapeDaoImpl.java
@@ -0,0 +1,61 @@
+package cz.fi.muni.pa165.dao;
+
+import cz.fi.muni.pa165.entity.Grape;
+import cz.fi.muni.pa165.entity.Wine;
+import cz.fi.muni.pa165.enums.GrapeColor;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+
+/**
+ * Implementation of the GrapeDao interface
+ *
+ * @author Lukáš Fudor
+ */
+public class GrapeDaoImpl implements GrapeDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void create(Grape grape) {
+        em.persist(grape);
+    }
+
+    @Override
+    public List<Grape> findAll() {
+        return em.createQuery("select g from Grape g", Grape.class).getResultList();
+    }
+
+    @Override
+    public Grape findById(Long id) {
+        return em.find(Grape.class, id);
+    }
+
+    @Override
+    public Grape findByName(String name) {
+        return em.createQuery("select g from Grape g where name=:name", Grape.class)
+                .setParameter("name", name)
+                .getSingleResult();
+    }
+
+    @Override
+    public List<Grape> findByColor(GrapeColor color) {
+        return em.createQuery("select g from Grape g where color=:color", Grape.class)
+                .setParameter("color", color)
+                .getResultList();
+    }
+
+    @Override
+    public void remove(Grape grape) {
+        em.remove(grape);
+    }
+
+    @Override
+    public void update(Grape grape) {
+        em.merge(grape);
+    }
+
+}
