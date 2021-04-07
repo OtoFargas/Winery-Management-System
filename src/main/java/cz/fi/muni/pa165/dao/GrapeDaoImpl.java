9722de8f340a70f3f99d6e64c0b288b97bf1f1bd
Lukas Fudor <fudor.lukas@gmail.com>
2021-04-07 14:19:31 +0200

    added repository annotation

--- src/main/java/cz/fi/muni/pa165/dao/GrapeDaoImpl.java
+++ src/main/java/cz/fi/muni/pa165/dao/GrapeDaoImpl.java
@@ -1,8 +1,8 @@
 package cz.fi.muni.pa165.dao;
 
-import cz.fi.muni.pa165.entity.Grape;
-import cz.fi.muni.pa165.entity.Wine;
+import cz.fi.muni.pa165.entities.Grape;
 import cz.fi.muni.pa165.enums.GrapeColor;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -14,6 +14,7 @@ import java.util.List;
  *
  * @author Lukáš Fudor
  */
+@Repository
 public class GrapeDaoImpl implements GrapeDao {
 
     @PersistenceContext
