d88a640ff3b4ccd37443026cd048c3274bb5eeaf
Lukas Fudor <fudor.lukas@gmail.com>
2021-04-25 09:40:38 +0200

    WineDTO and enums

---
+++ winery-api/src/main/java/cz/fi/muni/pa165/enums/Ingredient.java
@@ -0,0 +1,11 @@
+package cz.fi.muni.pa165.enums;
+
+/**
+ * Enum representing the possible ingredients in wine
+ *
+ * @author Vladimir Visnovsky
+ */
+
+public enum Ingredient {
+    POTASSIUM, CALCIUM, SULFUR, SUGAR, GRAPE_JUICE, WATER, OAK, VANILLA, TANNINS, YEAST,
+}
