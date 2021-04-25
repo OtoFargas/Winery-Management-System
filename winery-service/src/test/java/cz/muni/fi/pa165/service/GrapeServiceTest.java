04f9740b6543a457327a9271301f5c0520747bef
Oto Fargas <xfargas@fi.muni.cz>
2021-04-25 17:24:08 +0200

    added GrapeService test class

---
+++ winery-service/src/test/java/cz/muni/fi/pa165/service/GrapeServiceTest.java
@@ -0,0 +1,15 @@
+package cz.muni.fi.pa165.service;
+
+import cz.muni.fi.pa165.dao.GrapeDao;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.BeforeMethod;
+
+
+/**
+ * @author Oto Fargas
+ */
+public class GrapeServiceTest extends AbstractTestNGSpringContextTests {
+
+}
