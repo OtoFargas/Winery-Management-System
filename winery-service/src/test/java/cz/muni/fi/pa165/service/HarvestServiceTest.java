93d20281d370679e2be4149b0b929974019b233c
Lukas Fudor <fudor.lukas@gmail.com>
2021-04-25 17:32:19 +0200

    empty HarvestServiceTest

---
+++ winery-service/src/test/java/cz/muni/fi/pa165/service/HarvestServiceTest.java
@@ -0,0 +1,12 @@
+package cz.muni.fi.pa165.service;
+
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+/**
+ *  Test class for HarvestService
+ *
+ * @author Lukáš Fudor
+ */
+
+public class HarvestServiceTest extends AbstractTestNGSpringContextTests  {
+}
