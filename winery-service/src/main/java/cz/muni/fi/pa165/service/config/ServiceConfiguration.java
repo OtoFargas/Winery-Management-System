b1b2562ae09a9eea3eb7a4ba3a8db1af258a8a07
Lukas Fudor <fudor.lukas@gmail.com>
2021-04-25 18:36:59 +0200

    ServiceConfiguration

---
+++ winery-service/src/main/java/cz/muni/fi/pa165/service/config/ServiceConfiguration.java
@@ -0,0 +1,39 @@
+package cz.muni.fi.pa165.service.config;
+
+import cz.muni.fi.pa165.PersistenceApplicationContext;
+import cz.muni.fi.pa165.dto.GrapeDTO;
+import cz.muni.fi.pa165.entities.Grape;
+import cz.muni.fi.pa165.service.GrapeServiceImpl;
+import cz.muni.fi.pa165.service.facade.FeedbackFacadeImpl;
+import org.dozer.loader.api.BeanMappingBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+
+@Configuration
+@Import(PersistenceApplicationContext.class)
+@ComponentScan(basePackageClasses={GrapeServiceImpl.class, FeedbackFacadeImpl.class})
+public class ServiceConfiguration {
+
+    @Bean
+    public Mapper dozer(){
+        DozerBeanMapper dozer = new DozerBeanMapper();
+        dozer.addMapping(new DozerCustomConfig());
+        return dozer;
+    }
+
+    /**
+     * Custom config for Dozer if needed
+     * @author nguyen
+     *
+     */
+    public class DozerCustomConfig extends BeanMappingBuilder {
+        @Override
+        protected void configure() {
+            mapping(Grape.class, GrapeDTO.class);
+        }
+    }
+}
--- winery-service/src/test/java/cz/muni/fi/pa165/service/HarvestServiceTest.java
+++ winery-service/src/test/java/cz/muni/fi/pa165/service/HarvestServiceTest.java
@@ -1,5 +1,7 @@
 package cz.muni.fi.pa165.service;
 
+import cz.muni.fi.pa165.service.config.ServiceConfiguration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 
 /**
@@ -8,5 +10,6 @@ import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
  * @author Lukáš Fudor
  */
 
+@ContextConfiguration(classes= ServiceConfiguration.class)
 public class HarvestServiceTest extends AbstractTestNGSpringContextTests  {
 }
