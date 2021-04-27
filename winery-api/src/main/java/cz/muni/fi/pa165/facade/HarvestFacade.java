e793ad9e5d6a338bc2c1e677ceba0e9f60a462c4
Vladimir Visnovsky <xvisnov1@fi.muni.cz>
2021-04-27 10:59:20 +0200

    added harvest facade impl and interface

---
+++ winery-api/src/main/java/cz/muni/fi/pa165/facade/HarvestFacade.java
@@ -0,0 +1,42 @@
+package cz.muni.fi.pa165.facade;
+
+import java.util.List;
+
+import cz.muni.fi.pa165.dto.HarvestDTO;
+
+public interface HarvestFacade {
+    /**
+     * Create new harvest
+     * @param harvestDTO to be created
+     */
+    void createHarvest(HarvestDTO harvest);
+
+    /**
+     * Find all harvests
+     * @return list of all DTO harvests
+     */
+    List<HarvestDTO> findAllHarvests();
+
+    /**
+     * Find required harvest by it's id
+     * @param id of desired harvest
+     * @return found DTO harvest
+     */
+    HarvestDTO findHarvestById(Long id);
+
+    /**
+     * @param year of desired harvest
+     * @return list of found DTO harvests
+     */
+    List<HarvestDTO> findHarvestByYear(Integer year);
+
+    /**
+     * @param harvestDTO to be updated
+     */
+    void updateHarvest(HarvestDTO harvest);
+
+    /**
+     * @param harvestDTO to be deleted
+     */
+    void removeHarvest(HarvestDTO harvest);
+}
---
+++ winery-service/src/main/java/cz/muni/fi/pa165/service/facade/HarvestFacadeImpl.java
@@ -0,0 +1,77 @@
+package cz.muni.fi.pa165.service.facade;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import javax.inject.Inject;
+import org.springframework.stereotype.Service;
+
+import cz.muni.fi.pa165.dto.HarvestDTO;
+import cz.muni.fi.pa165.entities.Grape;
+import cz.muni.fi.pa165.entities.Harvest;
+import cz.muni.fi.pa165.entities.Wine;
+import cz.muni.fi.pa165.facade.HarvestFacade;
+import cz.muni.fi.pa165.service.BeanMappingService;
+import cz.muni.fi.pa165.service.HarvestService;
+
+
+/**
+ * @author Vladimir Visnovsky
+ */
+@Transactional
+@Service
+public class HarvestFacadeImpl implements HarvestFacade {
+
+    private final HarvestService harvestService;
+
+    private final BeanMappingService beanMappingService;
+
+    @Inject
+    public HarvestFacadeImpl(HarvestService harvestService, BeanMappingService beanMappingService) {
+        this.harvestService = harvestService;
+        this.beanMappingService = beanMappingService;
+    }
+
+    @Override
+    public void createHarvest(HarvestDTO harvestDTO) {
+        Harvest harvest = beanMappingService.mapTo(harvestDTO, Harvest.class);
+        harvestService.createHarvest(harvest);
+    }
+
+    @Override
+    public List<HarvestDTO> findAllHarvests() {
+        return beanMappingService.mapTo(harvestService.findAllHarvests(), HarvestDTO.class);
+    }
+
+    @Override
+    public HarvestDTO findHarvestById(Long id) {
+        Harvest harvest = harvestService.findHarvestById(id);
+        return (harvest == null) ? null : beanMappingService.mapTo(harvest, HarvestDTO.class);
+    }
+
+    @Override
+    public List<HarvestDTO> findHarvestByYear(Integer year) {
+        return beanMappingService.mapTo(harvestService.findHarvestByYear(year), HarvestDTO.class);
+    }
+
+    @Override
+    public void updateHarvest(HarvestDTO harvestDTO) {
+        Harvest harvest = harvestService.findHarvestById(harvestDTO.getId());
+
+        harvest.setHarvestYear(harvestDTO.getHarvestYear());
+        harvest.setQuality(harvestDTO.getQuality());
+        harvest.setQuantity(harvestDTO.getQuantity());
+        harvest.setGrape(beanMappingService.mapTo(harvestDTO.getGrape(), Grape.class));
+        harvest.setWine(beanMappingService.mapTo(harvestDTO.getWine(), Wine.class));
+
+        beanMappingService.mapTo(harvestDTO, Harvest.class);
+    }
+
+    @Override
+    public void removeHarvest(HarvestDTO harvestDTO) {
+        harvestService.removeHarvest(beanMappingService.mapTo(harvestDTO, Harvest.class));
+    }
+
+    
+}
