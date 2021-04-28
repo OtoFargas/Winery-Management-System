7477777b93cc8aee9eebb1eb9cc1ab28f3329c2b
Lukas Fudor <fudor.lukas@gmail.com>
2021-04-28 22:16:38 +0200

    FeedbackFacade complete tests

--- winery-service/src/test/java/cz/muni/fi/pa165/service/facade/FeedbackFacadeTest.java
+++ winery-service/src/test/java/cz/muni/fi/pa165/service/facade/FeedbackFacadeTest.java
@@ -17,6 +17,7 @@ import javafx.util.Pair;
 import org.mockito.InjectMocks;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -26,6 +27,7 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -71,6 +73,7 @@ public class FeedbackFacadeTest extends AbstractTestNGSpringContextTests {
         feedback.setContent("šmaky šmak");
         feedback.setDate(new Date());
         feedback.setWine(wine);
+        feedback.setId(17L);
 
         feedbackCreateDTO = new FeedbackCreateDTO();
         feedbackCreateDTO.setAuthor("Šakul Roduf");
@@ -80,22 +83,56 @@ public class FeedbackFacadeTest extends AbstractTestNGSpringContextTests {
         feedbackCreateDTO.setWine(beanMappingService.mapTo(wine, WineDTO.class));
 
         feedbackDTO = new FeedbackDTO();
-        //feedbackDTO.setAuthor();
+        feedbackDTO.setAuthor("Volodomir Cherry");
+        feedbackDTO.setRating(9);
+        feedbackDTO.setDate(new Date());
+        feedbackDTO.setContent("noice");
+        feedbackDTO.setWine(beanMappingService.mapTo(wine, WineDTO.class));
+        feedbackDTO.setId(11L);
     }
 
     @Test
     public void testCreateFeedback() {
         feedback = beanMappingService.mapTo(feedbackCreateDTO, Feedback.class);
-        feedbackFacade.createFeedback(feedbackCreateDTO);
+        Long id = feedbackFacade.createFeedback(feedbackCreateDTO);
         verify(feedbackService).createFeedback(feedback);
+        assertThat(id).isEqualTo(feedback.getId());
     }
 
     @Test
     public void testFindFeedbackById() {
         when(feedbackService.findFeedbackById(feedback.getId())).thenReturn(feedback);
-        feedbackFacade.findFeedbackById(feedback.getId());
+        FeedbackDTO testFeedbackDTO = feedbackFacade.findFeedbackById(feedback.getId());
         verify(feedbackService).findFeedbackById(feedback.getId());
+        assertThat(testFeedbackDTO).isEqualTo(beanMappingService.mapTo(feedback, FeedbackDTO.class));
     }
 
+    @Test
+    public void testFindAllFeedbacks() {
+        when(feedbackService.findAllFeedbacks()).thenReturn(new ArrayList<>(List.of(feedback)));
+        List<FeedbackDTO> feedbacks = feedbackFacade.findAllFeedbacks();
+        verify(feedbackService).findAllFeedbacks();
+        assertThat(feedbacks).containsExactlyInAnyOrder(beanMappingService.mapTo(feedback, FeedbackDTO.class));
+    }
 
+    @Test
+    public void testFindFeedbacksByAuthor() {
+        when(feedbackService.findFeedbackByAuthor("Foto Argaš")).thenReturn(new ArrayList<>(List.of(feedback)));
+        List<FeedbackDTO> feedbacks = feedbackFacade.findFeedbacksByAuthor("Foto Argaš");
+        verify(feedbackService).findFeedbackByAuthor("Foto Argaš");
+        assertThat(feedbacks).containsExactlyInAnyOrder(beanMappingService.mapTo(feedback, FeedbackDTO.class));
+    }
+
+    @Test
+    public void testUpdateFeedback() {
+        feedbackFacade.updateFeedback(feedbackDTO);
+        verify(feedbackService).updateFeedback(beanMappingService.mapTo(feedbackDTO, Feedback.class));
+    }
+
+    @Test
+    public void testRemoveFeedback() {
+        when(feedbackService.findFeedbackById(feedbackDTO.getId())).thenReturn(beanMappingService.mapTo(feedbackDTO, Feedback.class));
+        feedbackFacade.removeFeedback(feedbackDTO.getId());
+        verify(feedbackService).removeFeedback(beanMappingService.mapTo(feedbackDTO, Feedback.class));
+    }
 }
