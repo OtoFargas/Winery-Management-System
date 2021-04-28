8a08abfef0fa8c3c8b9fb77a2b73569b80278e6f
Oto Fargas <xfargas@fi.muni.cz>
2021-04-28 08:45:29 +0200

    created test class for harvest facade

---
+++ winery-service/src/test/java/cz/muni/fi/pa165/service/facade/HarvestFacadeTest.java
@@ -0,0 +1,11 @@
+package cz.muni.fi.pa165.service.facade;
+
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+/**
+ * @author Oto Fargas
+ */
+public class HarvestFacadeTest extends AbstractTestNGSpringContextTests {
+
+    
+}
