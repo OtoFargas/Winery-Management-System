d2d968f15a3287f53f5673110a8d128a31230e2a
Vladimir Visnovsky <xvisnov1@fi.muni.cz>
2021-05-18 15:12:38 +0200

    added body to exception class

--- winery-rest/src/main/java/cz/muni/fi/pa165/rest/exceptions/ResourceNotFoundException.java
+++ winery-rest/src/main/java/cz/muni/fi/pa165/rest/exceptions/ResourceNotFoundException.java
@@ -9,5 +9,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason="The requested resource was not found")
 public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
     
 } 
