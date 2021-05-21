d62db22fc14caa60fbd251f410cac44510b45952
Vladimir Visnovsky <xvisnov1@fi.muni.cz>
2021-05-21 16:11:41 +0200

    added null check bc of create harvest

--- winery-service/src/main/java/cz/muni/fi/pa165/service/facade/HarvestFacadeImpl.java
+++ winery-service/src/main/java/cz/muni/fi/pa165/service/facade/HarvestFacadeImpl.java
@@ -43,7 +43,11 @@ public class HarvestFacadeImpl implements HarvestFacade {
     @Override
     public Long createHarvest(HarvestCreateDTO harvestCreateDTO) {
         Harvest harvest = beanMappingService.mapTo(harvestCreateDTO, Harvest.class);
-        harvest.setWine(wineService.findWineById(harvestCreateDTO.getWineId()));
+        Long wineId = harvestCreateDTO.getWineId();
+
+        if (wineId != null) {
+            harvest.setWine(wineService.findWineById(wineId));
+        }
         harvest.setGrape(grapeService.findGrapeById(harvestCreateDTO.getGrapeId()));
         harvestService.createHarvest(harvest);
         return harvest.getId();
